d94d50c5b42176deabb8221d45006bafffc6cd83
Omar Ayman <se.omarayman@gmail.com>
2022-02-28 03:30:17 +0200

    restored package.json and put project in src folder

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "transpileDependencies": [
+    "vuetify"
+  ],
+
+
+
+  pluginOptions: {
+    i18n: {
+      locale: '(en)',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    },
+    webpackBundleAnalyzer: {
+      openAnalyzer: true
+    }
+  }
+}
